39c011bf4925661ae82c8cb88df58e51e67fd840
Brian McIntosh <brian@mixinteractive.com>
2023-01-28 15:32:54 -0500

    Added functions to get image data
    Wrote four functions to test different ways of get data from the network. Tests threading (queues)

--- Multithreading & Async/ViewController.swift
+++ Multithreading & Async/ViewController.swift
@@ -3,17 +3,80 @@
 //  Multithreading & Async
 //
 //  Created by Brian McIntosh on 1/28/23.
-//
+//  https://www.youtube.com/watch?v=C8yByOh8-6U
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getRandomImage(_ sender: Any) {
+        let url = URL(string: "https://loremflickr.com/2000/2000")!
+        let data = try! Data(contentsOf: url)
+        /*
+         This causes the UI to freeze
+         each thing happens sequentially one after the other
+         the next step can't happen until the previous step finishes downloading
+         The reason is b/c we're doing this on the main thread
+         e.g. an algorithm or saving data to file, anything that can take time
+         */
+        let image = UIImage(data: data)
+        self.imageView.image = image
+    }
+    
+    /*
+     in iOS, we don't think about threads, we think about queues
+     DispatchQueue is part of Grand Central Dispatch
+     */
+    @IBAction func getRandomImage2(_ sender: Any) {
+        DispatchQueue.global(qos: .background).async {
+            let url = URL(string: "https://loremflickr.com/2000/2000")!
+            let data = try! Data(contentsOf: url)
+            let image = UIImage(data: data)
+            /*
+             This will cause a crash!
+             Trying to update UI from a background thread (queue)
+             */
+            self.imageView.image = image
+        }
+    }
+    
+    @IBAction func getRandomImage3(_ sender: Any) {
+        DispatchQueue.global(qos: .background).async {
+            let url = URL(string: "https://loremflickr.com/2000/2000")!
+            let data = try! Data(contentsOf: url)
+            let image = UIImage(data: data)
+            /*
+             Fixes the crash
+             */
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+    }
+    
+    /*
+     FINALLY, we never download data this way! Use URLSession.
+     This actually takes care of some of the multithreading for us.
+     */
+    @IBAction func getRandomImage4(_ sender: Any) {
+        
+        let url = URL(string: "https://loremflickr.com/2000/2000")!
+        let task = URLSession.shared.downloadTask(with: url) { (localUrl, response, error) in
+            
+            let data = try! Data(contentsOf: localUrl!)
+            let image = UIImage(data: data)
+            
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+        task.resume()
+    }
 }
 
